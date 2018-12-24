26d4fa66fd3aabf2a6f7b7cd81a1c20940c0eafc
Andrew Heppner <andrewheppner@Andrews-MacBook-Pro-2.local>
2018-12-24 12:16:35 -0500

    Change text to test out the git webhook

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Beginning of the app</p>
+          <p>Beginning of the end of time</p>
         </header>
       </div>
     );
