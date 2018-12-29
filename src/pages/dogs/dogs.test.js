049f2bda33609cae0457c2b947ee250b692cd616
Andrew Heppner <andrewheppner@Andrews-MacBook-Pro-2.local>
2018-12-29 01:22:56 -0500

    Setup testing with enzyme and jest-enzyme. Create config.yml for circleci

---
+++ src/pages/dogs/dogs.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Dogs } from "./index";
+import { shallow } from "enzyme";
+
+it("renders the dogs page without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Dogs />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders the h1 element", () => {
+  const DogsComponent = shallow(<Dogs />);
+  const header = <h1>Dogs</h1>;
+  expect(DogsComponent).toContainReact(header);
+});
